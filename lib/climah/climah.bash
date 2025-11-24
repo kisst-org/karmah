a2aad3405f08830b4effe8ad4b6c3a050c68b520
Mark Hooijkaas <mark@hooijkaas.net>
2025-11-24 08:55:47 +0100

    added a check on bash version

--- lib/climah/climah.bash
+++ lib/climah/climah.bash
@@ -1,6 +1,16 @@
 
 
+check_bash_version() {
+    if [[ ${BASH_VERSINFO:-0} -lt 4 ]]; then
+        echo "bash version too old (3.x or older), please use a newer version"
+        echo "if you are on MacOS you can use the following command"
+        printf "  brew install bash\n"
+        exit 1
+    fi
+}
+
 climah_init() {
+    check_bash_version
     init_logging "${@}"
     init_all_modules
     read_config
