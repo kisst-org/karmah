dc40e3890ad37e63aaab80f402ba2ed2a46239fc
Mark Hooijkaas <mark@hooijkaas.net>
2026-04-17 16:00:46 +0200

    check immediately for bash version

--- lib/pre.bash
+++ lib/pre.bash
@@ -7,6 +7,13 @@
 # Install or update to newest version with following command:
 #   curl -OL https://raw.githubusercontent.com/kisst-org/karmah/refs/heads/main/karmah && chmod 755 karmah
 
+if [ ${BASH_VERSINFO:-0} -lt 4 ]; then
+    echo "bash version too old (3.x or older), please use a newer version"
+    echo "if you are on MacOS you can use the following command"
+    printf "  brew install bash\n"
+    exit 1
+fi
+
 set -eu
 shopt -s extglob
 script_name="${0}"
