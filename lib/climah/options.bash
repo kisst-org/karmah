077b89072b5581c05633042e04977f0421d844c8
Mark Hooijkaas <mark@hooijkaas.net>
2026-05-01 13:48:38 +0200

    streamlined value and  flag options

--- lib/climah/options.bash
+++ lib/climah/options.bash
@@ -1,6 +1,6 @@
 
 options::declare-vars() {
-    declare -gA option_type=()
+    declare -gA option_func=()
     declare -gA option_value=()
 }
 
@@ -8,38 +8,45 @@ options::init-module() {
     append-argparse-func parse-if-option
 }
 
-
 options-show-help() { list-help-items option; }
+
+get-option-var() {
+    local name=$1 default=${2:-}
+    local opt_name=${name//_/-}
+    echo "${option_value[$opt_name]:-$default}"
+}
+use-option-var() { declare -g $1=$(get-option-var $1 $2); }
+
+set-option-value() {
+    local name=$1 value="$2"
+    log-verbose option "setting option $name to \"$value\""
+    option_value[$name]="$value"
+}
+
+parse-flag-option() { set-option-value $name ${value:-true}; }
+parse-value-option() {
+   local name=$1 value="$2"
+   if [[ -z $value ]]; then
+       value=$2
+       argparse_parse_count=2
+   fi
+   set-option-value $name "$value"
+}
 
 parse-if-option() {
     local arg=${1}
     arg=${arg#--}
-    if [[ $arg == $1 ]]; then
-        return
-    fi
+    if [[ $arg == $1 ]]; then return 0; fi  # not an argument starting with --...
+
     local name=${arg/=*/}
+    local func=${option_func[$name]:-}
+    if [[ -z $func ]]; then return 0; fi   # not a known option
+
     local value="${arg/*=/}"
+    if [[ $value == $arg ]]; then value=""; fi   # TODO: this will not work with opt=  to set something to empty
+
     argparse_parse_count=1
-    case ${option_type[$name]:-none} in
-        value)
-            if [[ $value == $arg ]]; then # not format --opt=val
-                option_value[$name]="${2}"
-                argparse_parse_count=2
-            else
-                option_value[$name]="$value"
-                help_items_to_show+=" --$name"
-            fi
-            ;;
-        flag)
-            if [[ $value == $arg ]]; then # not format --opt=val
-                option_value[$name]=true
-            else
-                option_value[$name]="$value"
-                help_items_to_show+=" --$name"
-            fi
-            ;;
-        none)    argparse_parse_count=0;;
-    esac
+    $func $name $value "$@"
 }
 
 
@@ -55,13 +62,13 @@ add-parse-option()  { add-func-option "$1" $2 "$3" parse-option-$2 "$4"; }
 add-flag-option()   {
     local short=$1 name=$2 summary="$3"
     if [[ ! -z $short ]]; then argparse-add-short -$short --$name; fi
-    option_type[$name]=flag
+    option_func[$name]=parse-flag-option
     add-help-item --$name option:--$name "" "$summary"
 }
 add-value-option()   {
     local short=$1 name=$2 arg=$3 summary="$4"
     if [[ ! -z $short ]]; then argparse-add-short -$short --$name; fi
-    option_type[$name]=value
+    option_func[$name]=parse-value-option
     add-help-item --$name option:--$name "$arg" "$summary"
 }
 
