544f66e875c6b3e74e9e5b28265f894b40bb872d
Mark Hooijkaas <mark@hooijkaas.net>
2026-05-05 16:10:16 +0200

    kube resource functions

--- lib/modules/kube.bash
+++ lib/modules/kube.bash
@@ -1,24 +1,26 @@
 kube::init-module() {
     add-module-help "helper actions to work with kubernetes"
     add-karmah-var R replicas nr  "specify number of replicas"
-#TODO    add-karmah-var r resource res "specify a resource"
+    add-karmah-var r resource res "specify a resource"
 
-    add-karmah-action kw kube-watch   "watch target resources every 2 seconds"
-    add-karmah-action ""  kube-get       "get current manifests from cluster to --to <path> (default) deployed/manifests"
-    add-karmah-action ""  kube-tmp-scale "scale resource(s) without changing source or deployment files"
-    add-karmah-action ""  kube-restart   "restart resource(s)"
-    add-karmah-action ""  kube-env       "show the environment vars of a pod (run env in a shell)"
-    add-karmah-action kl  kube-log       "show logging of a resource"
+    add-karmah-action kw   kube-watch        "watch target resources every 2 seconds"
+    add-karmah-action kl   kube-log          "show logging of a resource"
+    add-karmah-action kg   kube-get          "get current manifests from cluster to --to <path> (default) deployed/manifests"
+    add-karmah-action ""   kube-scale        "scale resource(s) without changing source or deployment files"
+    add-karmah-action ""   kube-restart      "rollout restart resource(s)"
+    add-karmah-action kenv kube-env          "show the environment vars of a pod (run env in a shell)"
+    add-karmah-action kup  kube-uptime       "run the uptime commando on a pod"
     help_level=expert
-    add-karmah-action k   kubectl        "generic kubectl in the right cluster and namespace of all targets"
+    add-karmah-action k   kubectl            "generic kubectl in the right cluster and namespace of all targets"
     #add-karmah-action ks  kube-status    "show status of relevant resources"
-    add-karmah-action ke  kube-exec      "execute a command on a pod of a resource"
-    add-karmah-action kei kube-exec-it   "execute interactive command on a pod of a resource"
-    add-karmah-action kst kube-stern     "use stern to show logging of multiple pods"
+    add-karmah-action ke  kube-exec          "execute a command on a pod of a resource"
+    add-karmah-action kei kube-exec-it       "execute interactive command on a pod of a resource"
+    add-karmah-action kst kube-stern         "use stern to show logging of multiple pods"
+    add-karmah-action kgm kube-get-manifests "get current manifests from cluster to --to <path> (default) deployed/manifests"
 
-    add-karmah-var "" kube_config  "<file>" "The kube config file to be used (default means none)"
-    add-karmah-var "" kube_context "<ctx>"  "The kube context"
-    add-karmah-var "" kube_namespace "<ns>" "The kube namespace"
+    add-karmah-var "" kube_config  "file" "The kube config file to be used (default means none)"
+    add-karmah-var "" kube_context "ctx"  "The kube context"
+    add-karmah-var "" kube_namespace "ns" "The kube namespace"
     #local_vars+=" kube_config kube_context kube_namespace"
 }
 
@@ -64,7 +66,7 @@ action::kube-get-manifests() {
 }
 
 action::kube-get() {
-    run-cmd-from-action verbose kubectl $(kubectl-options) get ${action_args:-pods,deploy,sts,cm}
+    run-cmd-from-action verbose kubectl $(kubectl-options) get $(kube-calc-resource kube-get) ${action_args:-}
 }
 action::kube-watch() {
     run-cmd-from-action verbose watch kubectl $(kubectl-options) get $(kube-calc-resource kube-watch pods,deploy,sts,cm,svc,ingress,pdb) ${action_args:-}
@@ -74,6 +76,9 @@ action::kube-exec() {
 }
 action::kube-env() {
     run-cmd-from-action verbose kubectl $(kubectl-options) exec $(kube-calc-resource kube-exec) -- sh -c env
+}
+action::kube-uptime() {
+    run-cmd-from-action verbose kubectl $(kubectl-options) exec $(kube-calc-resource kube-exec) -- uptime
 }
 
 action::kube-exec-it() {
@@ -94,7 +99,7 @@ action::kube-restart() {
         run-cmd-from-action verbose kubectl $(kubectl-options) rollout restart $res
     done
 }
-action::kube-tmp-scale() {
+action::kube-scale() {
     local res
     for res in $(kube-calc-resource-names all); do
         local repl=$(kube-calc-replicas $res)
@@ -119,32 +124,34 @@ kube-calc-resource-names() {
     echo ${result//,/ }
 }
 
-kube-calc-full-resource-names() {
-    local r result="" res=${kube_resource_list:-${1:-}}
-    if [[ $res == all ]]; then
-        res=${kube_all_resources}
+_echo_if_func_exsists() {
+    if  $(function-exists $1); then
+        echo $1
+        return 0
     fi
-    for r in ${res//,/ }; do
-        result+=" ${kube_resource_alias[$r]:-$r}"
-    done
-    echo ${result//,/ }
+    return 1
 }
 
-
-render-kustomize() {
-    local command="kubectl kustomize --enable-helm"
-    #used_files+=" $helm_chart_dir/$ch"
-    run-verbose-cmd kubectl "$command ${karmah_dir}" \| split-yaml-docs-into-files
+kube-calc-resource-func() {
+    local func=""
+    if   _echo_if_func_exsists $karmah_type::calc-$action-resource; then return
+    elif _echo_if_func_exsists $karmah_type::calc-kube-resource;    then return
+    elif _echo_if_func_exsists        empty::calc-$action-resource; then return
+    elif _echo_if_func_exsists        empty::calc-kube-resource;    then return
+    fi
 }
-
 kube-calc-resource() {
     local action=$1 defaults="${2:-}"
-    if  [[ $(type -t ${karmah_type}::$action-resource) == function ]]; then
-        ${karmah_type}::$action-resource
-    elif [[ $(type -t ${karmah_type}::calc-kube-resource) == function ]]; then
-        ${karmah_type}::calc-kube-resource
-    else
-        echo $defaults
-    fi
+    local func=$(kube-calc-resource-func)
+    use-karmah-var resource default
+    local result=""
+    local res; for res in ${resource//,/ }; do
+        result+=" $($func $res)"
+    done
+    result=${result# } # trim leading space
+    echo ${result// /,}
 }
+ifed-combi::calc-kube-watch-resource() { ifed-combi::calc-kube-get-resource; }
+
+
 empty::calc-kube-resource() { echo ${kube_resource:-deployment}; }
