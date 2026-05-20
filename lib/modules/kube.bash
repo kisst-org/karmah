67ce7e2995ae0d6f9c3fba90a934761f15d67290
Mark Hooijkaas <mark@hooijkaas.net>
2026-05-20 19:58:09 +0200

    fixed kube-es-sync param order

--- lib/modules/kube.bash
+++ lib/modules/kube.bash
@@ -106,7 +106,7 @@ action::kube-log-follow() {
 }
 action::kube-es-sync() {
     # see https://external-secrets.io/latest/introduction/faq/
-    run-kubectl annotate es force-sync=$(date +%s) --overwrite $(kube-calc-resource kube-es-sync) "${@}"
+    run-kubectl annotate $(kube-calc-resource kube-es-sync) force-sync=$(date +%s) --overwrite  "${@}"
 }
 
 
