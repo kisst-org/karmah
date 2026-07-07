9bd73d3b8351386add1b4894794cc1bab3e68ff6
Mark Hooijkaas <mark@hooijkaas.net>
2026-07-07 11:35:58 +0200

    added --check option to run only specific (read-only) command

--- lib/climah/log-cmd.bash
+++ lib/climah/log-cmd.bash
@@ -2,6 +2,7 @@
 log-cmd::init-module() {
     add-func-option S show-script "" "show script of commands that would be executed"
     add-flag-option D dry-run        "do not execute the actual commands"
+    add-flag-option C check          "check-mode only run commands that will not change anything"
 }
 
 option::show-script() {
@@ -16,12 +17,24 @@ run-verbose-cmd() {
     if [[ ! -z ${module:-} ]]; then
         logger+=".${module}"
     fi
-    local dry_run=$(get-option-value dry-run false)
+    local do_run=true
+    local prefix=""
+    if $(get-option-value dry-run false); then
+        do_run=false
+    elif $(get-option-value check false); then
+        if ${run_in_check_mode:-false}; then
+            run_in_check_mode=false # reset so every call needs to set explicitely
+            do_run=true
+        else
+            prefix="# "  # prefix to show it is not really run
+            do_run=false
+        fi
+    fi
     local logmsg
     printf -v logmsg "%q " "$@"
-    log-at-level verbose $logger "$logmsg"
+    log-at-level verbose $logger "$prefix$logmsg"
     cmd_exit_code=0
-    if ! ${dry_run:-false}; then
+    if ${do_run}; then
         pipe=${cmd/*|/}
         cmd=${cmd/|*/}
         if ${ignore_cmd_exit_code:-false}; then
