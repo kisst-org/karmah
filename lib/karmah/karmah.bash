81ed8468264e20c251c5a99775bea43bac59174e
Mark Hooijkaas <mark@hooijkaas.net>
2026-08-20 11:57:51 +0200

    remove unset global_vars

--- lib/karmah/karmah.bash
+++ lib/karmah/karmah.bash
@@ -99,10 +99,6 @@ load-karmah-file() {
         log-info karmah "skipping $karmah_file"
         return
     fi
-    # cleanup of any vars that might have been set with previous file
-    log-trace karmah "clearing $local_vars"
-    unset $local_vars
-    declare -g $local_vars
 
     karmah_dir=$(dirname $karmah_file)
     common_dir=$(dirname $karmah_dir)/common
